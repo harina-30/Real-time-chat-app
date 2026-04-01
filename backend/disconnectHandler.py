70768daf6e98bf28aa2c15c0899e8b86ae080ba5
harina-30 <hmaveerkumar30@gmail.com>
2026-04-01 10:04:22 +0530

    Add disconnect handler for WebSocket connections

---
+++ backend/disconnectHandler.py
@@ -0,0 +1,13 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Connections')
+
+def lambda_handler(event, context):
+    connection_id = event['requestContext']['connectionId']
+
+    table.delete_item(
+        Key={'connectionId': connection_id}
+    )
+
+    return {'statusCode': 200}
